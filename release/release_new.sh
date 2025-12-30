90bec26750c21be02fbc069731e088619799392a
Guilhem Vellut <guilhem.vellut@gmail.com>
2025-12-30 18:29:51 +0100

    Add v before release tag

--- release/release_new.sh
+++ release/release_new.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-RELEASE_TAG="$1"
-
-if [ -z "$RELEASE_TAG" ]; then
+if [ -z "$1" ]; then
   echo "Error: No release tag provided."
   echo "Usage: $0 <tag_name>"
   exit 1
 fi
+
+RELEASE_TAG="v$1"
 
 # Check if the release already exists
 if gh release view "$RELEASE_TAG" > /dev/null 2>&1; then
